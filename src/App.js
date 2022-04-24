927c852e5e4af9012cb52353a58751e73180c2ba
Shovon Khan <shovon@commlinkinfotech.com.bd>
2022-04-25 03:10:34 +0600

    Initial ccommit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import Contact from "./components/pages/Contact";
+import Navbar from "./components/layout/Navbar";
+import NotFound from "./components/pages/NotFound";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import AddUser from "./components/users/AddUser";
+import Edit from "./components/users/Edit";
+import Viewuser from "./components/users/Viewuser";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+          <Route exact path="/contact" element={<Contact />} />
+          <Route exact path="/users/add" element={<AddUser />} />
+          <Route exact path="/users/edit/:id" element={<Edit />} />
+          <Route exact path="/users/viewuser/:id" element={<Viewuser />} />
+          <Route element={<NotFound />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
